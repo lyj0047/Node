93204aaf91d4842b176fdea6985b07b199bb8317
leeyujeong <lyj0047@naver.com>
2023-02-09 22:55:23 +0900

    err: mongoose

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://lyj0047:abcd1234@boilerplate.cdmrajq.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("MongoDV Connected..."))
+  .catch((err) => console.log(err));
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
